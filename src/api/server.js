7b01022066324a74241c421b1413575c11917ec4
Tianying0902 <Aimee_wang0902@163.com>
2023-02-28 11:25:30 +0800

    [Tianying]fix: change port and declare license

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = 3001;
+const port = 3333;
 app.listen(port, () => {
-  console.log("server listening on port:3001");
+  console.log(`server listening on port:${port}`);
 });
