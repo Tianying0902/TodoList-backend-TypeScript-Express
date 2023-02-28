5c1027b9991486deff8f7e95bb6c7bc89ad4c4f6
Tianying0902 <Aimee_wang0902@163.com>
2023-02-28 11:35:29 +0800

    [Tianying]fix: clear irrelevant dependencies amd ignore .vscode cofiguration

--- src/tests/promise.test.js
+++ src/tests/promise.test.js
@@ -1,19 +1,5 @@
 const queryPromise = require("../api/promise");
 const dbConnection = require("../api/dbConnection");
-const mockSql = jest.mock("mysql");
-
-const mysql = require("mysql");
-const mySqlConnection = () => {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    port: 1234,
-    user: "root",
-    password: "root1234",
-    database: "mockSql",
-  });
-  return connection;
-};
-
 const connectionMock = jest.fn(dbConnection);
 
 test("make sure connect to database successfully", async () => {
